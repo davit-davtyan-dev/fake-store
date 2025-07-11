25444a806ef3eb8f110904a385ea03fa6f732bfc
Davit Davtyan <davit.davtyan.dev@gmail.com>
2025-07-11 15:28:49 +0400

    Added add to cart button

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
 import {
   Card,
+  CardAction,
   CardContent,
   CardDescription,
   CardFooter,
@@ -19,14 +21,14 @@ export default function ProductCard({ product }: ProductCardProps) {
   const { id, title, category, image, price, rating } = product;
 
   return (
-    <Card className="justify-between">
+    <Card className="px-2 justify-between">
       <CardHeader>
         <CardTitle>
           <Link href={`/products/${id}`}>{title}</Link>
         </CardTitle>
         <CardDescription>{category}</CardDescription>
       </CardHeader>
-      <CardContent className="p-4">
+      <CardContent className="px-2 py-4">
         <div className="relative w-full h-32">
           <Image src={image} alt={title} fill className="object-contain mb-4" />
         </div>
@@ -49,6 +51,19 @@ export default function ProductCard({ product }: ProductCardProps) {
           {rating.rate} <span className="text-gray-500">({rating.count})</span>
         </span>
       </CardFooter>
+      <CardAction className="w-full">
+        <Button className="w-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6"
+          >
+            <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
+          </svg>
+          Add to Cart
+        </Button>
+      </CardAction>
     </Card>
   );
 }
